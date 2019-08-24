2314ec967c2c3491fad0d41de63ed1238a68b0ab
LeGriffon <haoyuan.wang@sjsu.edu>
2019-08-24 10:46:30 -0700

    commit Q36

---
+++ [0036][Valid Sudoku]/src/Solution.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i < 9; i++) {
+            HashSet<Character> rows = new HashSet<>();
+            HashSet<Character> cols = new HashSet<>();
+            HashSet<Character> box = new HashSet<>();
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.' && !rows.add(board[i][j])) {
+                    return false;
+                }
+                if(board[j][i] != '.' && !cols.add(board[j][i])) {
+                    return false;
+                }
+                int rowIndex = 3*(i/3);
+                int colIndex = 3*(i%3);
+                if(board[rowIndex + j/3][colIndex + j%3] != '.' && !box.add(board[rowIndex + j/3][colIndex + j%3])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
