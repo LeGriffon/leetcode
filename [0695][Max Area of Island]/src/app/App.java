4965c0e479d695b1a738dfd6bfc2f255d5b41bcc
LeGriffon <haoyuan.wang@sjsu.edu>
2020-03-25 13:39:34 -0700

    commit Q695

---
+++ [0695][Max Area of Island]/src/app/App.java
@@ -0,0 +1,19 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[][] grid = {
+                        {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                        {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                        {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                        {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                        {0,0,0,0,0,0,0,1,1,0,0,0,0} 
+                    };
+        Solution obj = new Solution();
+        int result = obj.maxAreaOfIsland(grid);
+        System.out.println(result);
+    }
+}
