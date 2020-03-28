d1b4126096da829ac59d9c729d9cdae2e7bb3e31
LeGriffon <haoyuan.wang@sjsu.edu>
2020-03-28 14:20:13 -0700

    commit Q279

---
+++ [0279][Perfect Squares]/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        int result = obj.numSquares(13);
+        System.out.println(result);
+    }
+}
