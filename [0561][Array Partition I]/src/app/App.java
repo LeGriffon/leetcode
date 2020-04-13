0f09ebb6a8c50cae09815359860d6538c2e71f6d
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-13 12:22:46 -0700

    commit Q561

---
+++ [0561][Array Partition I]/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] nums = {1,4,3,2};
+        Solution obj = new Solution();
+        int result = obj.arrayPairSum(nums);
+        System.out.println(result);
+    }
+}
