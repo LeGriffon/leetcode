c267e6a6564ce20277423bccd8b55fdbed5b9cf9
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-19 18:15:53 -0700

    commit Q153

---
+++ [0153][Find Minimum in Rotated Sorted Array]/src/app/Solution.java
@@ -0,0 +1,25 @@
+package app;
+
+public class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int left = 0, right = nums.length - 1;
+        if(nums[left] < nums[right]) return nums[left];
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            else if(nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+            else if(nums[left] < nums[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+}
