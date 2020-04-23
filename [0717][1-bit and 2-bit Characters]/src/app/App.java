7c4007bfd0a7e1fb8b9fbd436906c15b16f5ba26
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-23 15:15:39 -0700

    commit Q717

---
+++ [0717][1-bit and 2-bit Characters]/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] bits = {1, 0, 0};
+        Solution obj = new Solution();
+        boolean result = obj.isOneBitCharacter(bits);
+        System.out.println(result);
+    }
+}
