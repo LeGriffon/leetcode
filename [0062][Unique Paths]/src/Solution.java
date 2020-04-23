85931728ce756240fb9adde2c181e87a2521ee0d
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-22 17:32:16 -0700

    update Q62

--- [0062][Unique Paths]/src/Solution.java
+++ [0062][Unique Paths]/src/Solution.java
@@ -1,29 +1,16 @@
+import java.util.*;
 
-public class Solution {
-	/*
-	 * We have to get the total number of permutations of the given path. So,
-	 * decrease both m & n by 1 and apply following formula:
-	 * 
-	 * Total permutations = (m+n)! / (m! * n!)
-	 */
-	public int uniquePaths(int m, int n) {
-		if (m == 1 || n == 1) {
-			return 1;
-		}
-		if (m < n) {
-			int temp = m;
-			m = n;
-			n = temp;
-		}
-		m--;
-		n--;
-
-		long res = 1;
-		int n_tmp = 1;
-		for (int m_tmp = m + 1; m_tmp <= m + n; m_tmp++, n_tmp++) {
-			res *= m_tmp;
-			res /= n_tmp;
-		}
-		return (int) res;
-	}
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
+        for(int j = 1; j < n; j++) {
+            for(int i = 1; i < m; i++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
