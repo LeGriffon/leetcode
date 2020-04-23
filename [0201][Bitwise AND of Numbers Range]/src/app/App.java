e0b0a48fb84fea97c98de46d1f9ee570be60db70
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-23 14:49:03 -0700

    commit Q201

---
+++ [0201][Bitwise AND of Numbers Range]/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        int result = obj.rangeBitwiseAnd(5, 7);
+        System.out.println(result);
+    }
+}
