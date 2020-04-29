8a92cf3928bba5d6bc342fc3c419a759b8a100cc
LeGriffon <haoyuan.wang@sjsu.edu>
2020-04-29 15:45:29 -0700

    update Q105

--- [0105][Construct Binary Tree from Preorder and Inorder Traversal]/src/app/Solution.java
+++ [0105][Construct Binary Tree from Preorder and Inorder Traversal]/src/app/Solution.java
@@ -3,24 +3,33 @@ package app;
 import java.util.*;
 
 public class Solution {
+    private int[] preorder;
+    private int[] inorder;
+    private Map<Integer, Integer> indexMap;
+    private int preorderIndex;
+    
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> map = new HashMap<>();
+        this.preorder = preorder;
+        this.inorder = inorder;
+        this.indexMap = new HashMap<>();
+        this.preorderIndex = 0;
         for(int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
+            indexMap.put(inorder[i], i);
         }
-        return buildTreeHelper(0, preorder.length - 1, 0, inorder.length - 1, preorder, inorder, map);
+        return helper(0, inorder.length);
     }
-
-    private TreeNode buildTreeHelper(int preLeft, int preRight, int inLeft, int inRight, int[] preorder, int[] inorder, Map<Integer, Integer> map) {
-        if(preLeft > preRight || inLeft > inRight) return null;
-
-        TreeNode node = new TreeNode(preorder[preLeft]);
-        int currNodeInorderIndex = map.get(node.val);
-        int numNodesOnLeft = currNodeInorderIndex - inLeft;
-
-        node.left = buildTreeHelper(preLeft + 1, preLeft + numNodesOnLeft, inLeft, currNodeInorderIndex - 1, preorder, inorder, map);
-        node.right = buildTreeHelper(preLeft + numNodesOnLeft + 1, preRight, currNodeInorderIndex + 1, inRight, preorder, inorder, map);
-
+    
+    private TreeNode helper(int inorderLeft, int inorderRight) {
+        if(inorderLeft == inorderRight) {
+            return null;
+        }
+        
+        TreeNode node = new TreeNode(preorder[preorderIndex++]);
+        int inorderIndex = indexMap.get(node.val);
+        
+        node.left = helper(inorderLeft, inorderIndex);
+        node.right = helper(inorderIndex + 1, inorderRight);
+        
         return node;
     }
 }
