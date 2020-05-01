83cb6936879d74d1cf2d944eb03f99501e238ce4
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-01 13:44:09 -0700

    update Q67

--- [0067][Add Binary]/src/Solution.java
+++ [0067][Add Binary]/src/Solution.java
@@ -1,24 +1,16 @@
-
+import java.math.BigInteger;
 public class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int aLength = a.length() - 1;
-        int bLength = b.length() - 1;
-        int carry = 0;
-        while(aLength >=0 || bLength >=0) {
-            int sum = carry;
-            if(bLength >= 0) {
-                sum += b.charAt(bLength--) - '0';
-            }
-            if(aLength >= 0) {
-                sum += a.charAt(aLength--) - '0';
-            }
-            sb.append(sum % 2);
-            carry = sum / 2;
+        BigInteger x = new BigInteger(a, 2);
+        BigInteger y = new BigInteger(b, 2);
+        BigInteger zero = new BigInteger("0", 2);
+        BigInteger carry, result;
+        while(y.compareTo(zero) != 0) {
+            result = x.xor(y);
+            carry = x.and(y).shiftLeft(1);
+            x = result;
+            y = carry;
         }
-        if(carry != 0) {
-            sb.append(carry);
-        }
-        return sb.reverse().toString();
+        return x.toString(2);
     }
 }
