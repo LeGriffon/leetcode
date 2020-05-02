b607435ac940808b16c415c15091de30b2cbd66a
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-02 16:07:41 -0700

    commit Q273

---
+++ [0273][Integer to English Words]/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        String result = obj.numberToWords(1234567891);
+        System.out.println(result);
+    }
+}
