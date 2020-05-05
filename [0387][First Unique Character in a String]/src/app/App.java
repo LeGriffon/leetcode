f7d3d4e5fbe605360eb663c2730844ff829f1a5d
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-05 08:17:01 -0700

    commit Q387

---
+++ [0387][First Unique Character in a String]/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String s = "leetcode";
+        Solution obj = new Solution();
+        int result = obj.firstUniqChar(s);
+        System.out.println(result);
+    }
+}
