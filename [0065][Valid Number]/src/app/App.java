44c9d6f5f00c46081ab9988a982a0aaf1bcf6f58
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-09 14:33:15 -0700

    commit Q65

---
+++ [0065][Valid Number]/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String s = "95a54e53";
+        Solution obj = new Solution();
+        boolean result = obj.isNumber(s);
+        System.out.println(result);
+    }
+}
