6887fb9775954c6ef2ca3b25351bf9b81327ce9f
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-15 12:38:12 -0700

    commit Q125

---
+++ [0125][Valid Palindrome]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String s = "A man, a plan, a canal: Panama";
+        Solution obj = new Solution();
+        boolean result = obj.isPalindrome(s);
+        System.out.println(result);
+    }
+}
