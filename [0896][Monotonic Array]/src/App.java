67a7bb690b13280b697b6976c22d9da828d5d5f6
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-17 22:28:10 -0700

    commit Q896

---
+++ [0896][Monotonic Array]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] A = {6,5,4,4};
+        Solution obj = new Solution();
+        boolean result = obj.isMonotonic(A);
+        System.out.println(result);
+    }
+}
