60f5f85bbc23240d9516fd9f52dddb52cc5d3aee
LeGriffon <haoyuan.wang@sjsu.edu>
2020-05-28 12:13:15 -0700

    commit Q191

---
+++ [0191][Number of 1 Bits]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n = 10;
+        Solution obj = new Solution();
+        int result = obj.hammingWeight(n);
+        System.out.println(result);
+    }
+}
