0643f45ebb8a76cc34c17a1de3bb772a6e52e17e
LeGriffon <haoyuan.wang@sjsu.edu>
2020-06-08 15:28:10 -0700

    commit Q231

---
+++ [0231][Power of two]/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        boolean result = obj.isPowerOfTwo(8);
+        System.out.println(result);
+    }
+}
