933e0e1fc69d36dbb46dc643ffa6ac56a7b2644a
LeGriffon <haoyuan.wang@sjsu.edu>
2020-06-15 11:51:22 -0700

    commit Q700

---
+++ [0700][Search in a Binary Search Tree]/src/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val) {
+            root = (root.val > val) ? root.left : root.right;
+        }
+        return root;
+    }
+ }
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
