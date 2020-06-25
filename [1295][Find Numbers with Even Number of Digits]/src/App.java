b08f2df8fa80d131aa6ebd0d0553a2ddf7667b17
LeGriffon <haoyuan.wang@sjsu.edu>
2020-06-24 23:09:16 -0700

    commit Q1295

---
+++ [1295][Find Numbers with Even Number of Digits]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] nums = {12,345,2,6,7896};
+        Solution obj = new Solution();
+        int result = obj.findNumbers(nums);
+        System.out.println(result);
+    }
+}
