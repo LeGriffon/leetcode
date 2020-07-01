bc500b5605fd6181f974d996c53248e75856540e
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-01 14:32:34 -0700

    commit Q441

---
+++ [0441][Arranging Coins]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n = 5;
+        Solution obj = new Solution();
+        int result = obj.arrangeCoins(n);
+        System.out.println(result);
+    }
+}
