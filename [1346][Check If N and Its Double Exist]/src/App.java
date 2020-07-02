aaa8df3bc14a784e5f4fbb12766060c678ceee94
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-02 14:14:30 -0700

    commit Q1346

---
+++ [1346][Check If N and Its Double Exist]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] arr = {10, 2, 5, 3};
+        Solution obj = new Solution();
+        boolean result = obj.checkIfExist(arr);
+        System.out.println(result);
+    }
+}
---
+++ [1346][Check If N and Its Double Exist]/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : arr) {
+            if(set.contains(n * 2) || (set.contains(n / 2) && n % 2 == 0)) {
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
