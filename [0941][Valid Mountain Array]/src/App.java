b22eea20943df9fd94273be6664710ab1ad61b37
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-02 14:53:23 -0700

    commit Q941

---
+++ [0941][Valid Mountain Array]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        int[] A = {0, 3, 2, 1};
+        boolean result = obj.validMountainArray(A);
+        System.out.println(result);
+    }
+}
