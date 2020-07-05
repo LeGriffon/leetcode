a587f9f89f678aa830d5254b0a92565b8375b6b9
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-05 15:49:49 -0700

    commit Q263

---
+++ [0263][Ugly Number]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 6;
+        Solution obj = new Solution();
+        boolean result = obj.isUgly(num);
+        System.out.println(result);
+    }
+}
