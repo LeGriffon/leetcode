d1d687c277bc1554459eb154db6c22058b3eef76
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-05 15:17:33 -0700

    commit Q461

---
+++ [0461][Hamming Distance]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 1, y = 4;
+        Solution obj = new Solution();
+        int result = obj.hammingDistance(x, y);
+        System.out.println(result);
+    }
+}
