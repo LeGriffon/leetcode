4de85eaf07b0fe4ca772d32ea33484cf7ae13e2e
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-10 13:36:50 -0700

    commit Q430

---
+++ [0430][Flatten a Multilevel Doubly Linked List]/src/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public Node flatten(Node head) {
+        if(head == null) return null;
+        Node p = head;
+        while(p != null) {
+            if(p.child == null) {
+                p = p.next;
+                continue;
+            }
+            Node temp = p.child;
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = p.next;
+            if(p.next != null) {
+                p.next.prev = temp;
+            }
+            p.next = p.child;
+            p.child.prev = p;
+            p.child = null;
+        }
+        return head;
+    }
+}
+
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
