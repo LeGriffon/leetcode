ced9ba1a0d17e53377bc00f777d62676b285f624
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-12 18:33:39 -0700

    commit Q190

---
+++ [0190][Reverse Bits]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n = 43261596;
+        Solution obj = new Solution();
+        int result = obj.reverseBits(n);
+        System.out.println(result);
+    }
+}
