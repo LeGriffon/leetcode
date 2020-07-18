cde4a2cd1678d0c3a984856321833d874926da0e
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-17 21:33:56 -0700

    commit Q198

---
+++ [0198][House Robber]/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] nums = {2,7,9,3,1};
+        Solution obj = new Solution();
+        int result = obj.rob(nums);
+        System.out.println(result);
+    }
+}
