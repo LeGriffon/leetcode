a44349a1c92cfe016c3aa7b152ef0c5a423daa99
LeGriffon <haoyuan.wang@sjsu.edu>
2020-07-26 14:06:24 -0700

    commit Q258

---
+++ [0258][Add Digits]/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 38;
+        int result = Solution.addDigits(num);
+        System.out.println(result);
+    }
+}
