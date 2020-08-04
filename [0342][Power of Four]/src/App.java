d5f03e9f9e6bd3f7b82f45573c893dfaaa20d586
LeGriffon <haoyuan.wang@sjsu.edu>
2020-08-04 12:38:32 -0700

    commit Q342

---
+++ [0342][Power of Four]/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        boolean result = obj.isPowerOfFour(16);
+        System.out.println(result);
+    }
+}
