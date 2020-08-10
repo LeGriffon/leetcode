f4b189f3e00254121087364a4ce46ff3cb445e61
LeGriffon <haoyuan.wang@sjsu.edu>
2020-08-10 10:56:53 -0700

    commit Q171

---
+++ [0171][Excel Sheet Column Number]/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        int result = obj.titleToNumber("AA");
+        System.out.println(result);
+    }
+}
