8abba20f28eb8086154ea41755d11270fb816904
LeGriffon <haoyuan.wang@sjsu.edu>
2020-08-26 15:33:44 -0700

    commit Q204

---
+++ [0204][Count Primes]/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Solution obj = new Solution();
+        int result = obj.countPrimes(10);
+        System.out.println(result);
+    }
+}
