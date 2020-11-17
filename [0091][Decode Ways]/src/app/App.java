ba1611582c88129a451b2f84442261dfde21d370
LeGriffon <haoyuan.wang@sjsu.edu>
2020-11-17 14:34:56 -0800

    Update Q91

--- [0091][Decode Ways]/src/app/App.java
+++ [0091][Decode Ways]/src/app/App.java
@@ -4,7 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         String s = "226";
         Solution obj = new Solution();
+        Solution2 obj2 = new Solution2();
         int result = obj.numDecodings(s);
+        int result2 = obj2.numDecodings(s);
         System.out.println(result);
+        System.out.println(result2);
     }
 }
